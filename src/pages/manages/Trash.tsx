5548391d639c742cf65c5f64222f5680f97b41db
‘muddyrain’ <qiu3291002845@gmail.com>
2023-05-03 16:15:04 +0800

    feat: 回收站添加恢复删除2个按钮

--- src/pages/manages/Trash.tsx
+++ src/pages/manages/Trash.tsx
@@ -1,10 +1,12 @@
 import { useTitle } from 'ahooks'
 import { FC, useState } from 'react'
-import { Typography, Empty, Spin, Table, TableColumnsType, Tag } from 'antd'
+import { Typography, Empty, Spin, Table, TableColumnsType, Tag, Space, Button, Modal } from 'antd'
 import styles from './common.module.scss'
 import { ColumnsType } from 'antd/es/table'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
 
 const { Title } = Typography
+const { confirm } = Modal
 
 const rawQuestionList = [
   {
@@ -36,6 +38,8 @@ const rawQuestionList = [
 const Trash: FC = () => {
   useTitle('小慕问卷 - 回收站')
   const [questionList, setQuestionList] = useState(rawQuestionList)
+  // 记录选中的 id
+  const [selectedIds, setSelectedIds] = useState<string[]>([])
   const tableColumns: ColumnsType<(typeof questionList)[0]> = [
     {
       title: '标题',
@@ -57,6 +61,42 @@ const Trash: FC = () => {
       dataIndex: 'createdAt',
     },
   ]
+  const del = () => {
+    confirm({
+      title: '确认彻底删除该问卷？',
+      icon: <ExclamationCircleOutlined />,
+      content: '删除以后不可以找回',
+      onOk: () => {
+        console.log('删除')
+      },
+    })
+  }
+  const TableElement = (
+    <>
+      <div style={{ marginBottom: 16 }}>
+        <Space>
+          <Button type="primary" disabled={selectedIds.length === 0}>
+            恢复
+          </Button>
+          <Button danger disabled={selectedIds.length === 0} onClick={del}>
+            删除
+          </Button>
+        </Space>
+      </div>
+      <Table
+        columns={tableColumns}
+        dataSource={questionList}
+        pagination={false}
+        rowKey={p => p._id}
+        rowSelection={{
+          type: 'checkbox',
+          onChange: (selectedRowKeys, selectedRows) => {
+            setSelectedIds(selectedRowKeys as string[])
+          },
+        }}
+      />
+    </>
+  )
   return (
     <>
       <div className={styles.header}>
@@ -67,9 +107,7 @@ const Trash: FC = () => {
       </div>
       <div className={styles.content}>
         {questionList.length === 0 && <Empty description="暂无数据" />}
-        {questionList.length > 0 && (
-          <Table columns={tableColumns} dataSource={questionList} pagination={false} />
-        )}
+        {questionList.length > 0 && TableElement}
       </div>
     </>
   )
